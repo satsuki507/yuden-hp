63bf0ed2c83b0a2418cf9dfd2fcaf7e566b9736c
satsuki <hazamanokoakuma@gmail.com>
2026-03-09 00:11:33 +0900

    Environment setup, TOP image change, slight text enlargement

---
+++ src/libs/microcms.ts
@@ -0,0 +1,9 @@
+// src/libs/microcms.ts
+import { createClient } from "microcms-js-sdk";
+
+const client = createClient({
+    serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
+    apiKey: import.meta.env.MICROCMS_API_KEY,
+});
+
+export default client;
